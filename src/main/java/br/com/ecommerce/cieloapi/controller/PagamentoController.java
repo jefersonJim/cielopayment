6f6104e8e814a3922e258fab912b480bf74503a1
jefersonJim <jeferson.inacio.macedo@gmail.com>
2018-08-05 09:24:56 -0300

    mudança para post no controler

--- src/main/java/br/com/ecommerce/cieloapi/controller/PagamentoController.java
+++ src/main/java/br/com/ecommerce/cieloapi/controller/PagamentoController.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ public class PagamentoController {
 	@Autowired
 	private PagamentoService pagamentoService;
 	
-	@GetMapping(value="/cartao")
+	@PostMapping(value="/cartao")
 	public ResponseEntity<PaymentResponse> cartao(@RequestBody PaymentRequest payRequest) {
 		try {
 			return new ResponseEntity<PaymentResponse>(this.pagamentoService.pagamentoCartao(payRequest),HttpStatus.OK);
